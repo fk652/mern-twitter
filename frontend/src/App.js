5dc67de689204637ecf96ffc2931e1cd6e326cb6
Fahim Khan <fk652@nyu.edu>
2023-04-15 18:50:20 -0400

    add login signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,18 @@ import NavBar from './components/NavBar/NavBar';
 import MainPage from './components/MainPage/MainPage';
 import LoginForm from './components/SessionForms/LoginForm';
 import SignupForm from './components/SessionForms/SignupForm';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getCurrentUser } from './store/session';
 
 function App() {
-  return (
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentUser()).then(() => setLoaded(true));
+  }, [dispatch]);
+
+  return loaded && (
     <>
       <NavBar />
       <Switch>
