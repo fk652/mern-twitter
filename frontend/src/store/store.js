3399ee4267fca5bade0001c7cf4b78f550c62b2d
Fahim Khan <fk652@nyu.edu>
2023-04-15 19:20:27 -0400

    add tweets

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import session from './session';
 import errors from './errors';
+import tweetsReducer from './tweets';
+import sessionReducer from './session';
 
 const rootReducer = combineReducers({
-  session,
-  errors
+  session: sessionReducer,
+  errors,
+  tweets: tweetsReducer
 });
 
 let enhancer;
