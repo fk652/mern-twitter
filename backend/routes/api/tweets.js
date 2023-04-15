f35a6692ced50e1b7a1990c370ae1acd723f70d5
Fahim Khan <fk652@nyu.edu>
2023-04-15 13:10:05 -0400

    update tweets comment

--- backend/routes/api/tweets.js
+++ backend/routes/api/tweets.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
+/* GET tweets listing. */
 router.get('/', function(req, res, next) {
   res.json({
     message: "GET /api/tweets"
